67c8cc74e55e89e234a5c82318decf322c928172
Roman98nsk <yaroshev.roma@mail.ru>
2022-08-16 00:25:36 +0700

    Correct test for score

--- tests/Test_score.c
+++ tests/Test_score.c
@@ -1,43 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 int main() {
-    int score_left_player = 0, score_right_player = 0, score_part_left_player = 0, score_part_right_player = 0,
-                                                                                    score_difference = 0, part = 1;
+    int score_left_player = 0, score_right_player = 0, score_part_left_player = 0, score_part_right_player = 0, score_difference = 0, part = 1;
 
     do {
- printf("Input score left player (0, 1, 2): \n");
-    scanf("%d", &score_left_player);
+        srand(time(NULL));
+        score_left_player = 0 + rand()%12;
+        score_right_player = 0 + rand()%12;
 
-    printf("Input score right player (0, 1, 2): \n");
-    scanf("%d", &score_right_player);
+        score_difference = abs(score_left_player - score_right_player);
 
-    if (score_left_player > 2 || score_right_player > 2) {
-        printf("Incorrect input!\n");
-        return 0;
-    }
+        sleep(2);
 
-    sleep(2);
-    score_difference = score_left_player - score_right_player;
+        if (score_left_player != 11 || score_right_player != 11) {
+            printf("\t\tGame %d", part);
+            printf("\n\t\t\033[1:32m%d : %d\033[0m", score_left_player, score_right_player);
+            printf("\n    \"Left_player\" : \"Right_player\"\n");
+            printf("\t%d \t\t    %d\n", score_part_left_player, score_part_right_player);
+        }
 
-    if ((score_left_player != 2 || score_right_player != 2) || (abs(score_difference) < 2)) {
-        printf("\t       Part %d", part);
-        printf("\n\t\t\033[1:32m%d : %d\033[0m", score_left_player, score_right_player);
-        printf("\n    \"Left_player\" : \"Right_player\"\n");
-    }
+        if ((score_left_player == 11 || score_left_player > 11) && score_difference >= 2) {
+            printf("\n\033[1;32mLEFT PLAYER WON GAME!\nGAME SCORE %d : %d.\033[0m\n\n", score_left_player, score_right_player);
+            part++;
+            score_part_left_player++;
+        } else if ((score_right_player == 11 || score_right_player > 11) && score_difference >= 2) {
+            printf("\n\033[1;32mRIGHT PLAYER WON GAME!\nGAME SCORE %d : %d.\033[0m\n\n", score_left_player, score_right_player);
+            part++;
+            score_part_right_player++;
+        }
 
-    if (score_left_player == 2 && score_part_left_player == 7 && (abs(score_difference) >= 2)) {
-        printf("\t.");
-        printf("\n\033[1;32mCONGRATULATION!!! LEFT PLAYER IS WON. SCORE %d : %d.\033[0m\n\n", score_part_left_player, score_part_right_player);
-        return 0;
-    } else if (score_right_player == 2 && score_part_right_player == 7 && (abs(score_difference) >= 2)) {
-        printf("\t\t\t.");
-        printf("\n\033[1;32mCONGRATULATION!!! RIGHT PLAYER IS WON. SCORE %d : %d.\033[0m\n\n", score_part_left_player, score_part_right_player);
-        return 0;
-    }
-    part++;
-    } while ((score_left_player != 2 || score_right_player != 2) && (abs(score_difference) < 2) && part != 8);
-    
+        if (part > 7) {
+            printf("\033[1;32mTOTAL SCORE: %d : %d\033[0m\n", score_part_left_player, score_part_right_player);
+            if (score_part_left_player > score_part_right_player) {
+                printf("\n\033[1;32mCONGRATULATION!!! LEFT PLAYER CHAMPION!\033[0m\n\n");
+            } else {
+                printf("\n\033[1;32mCONGRATULATION!!! RIGHT PLAYER CHAMPION!\033[0m\n\n");
+            }
+            return 0;
+        }
+
+    } while ((score_left_player != 12 || score_right_player != 12) || score_difference < 2);
 return 0;
 }
